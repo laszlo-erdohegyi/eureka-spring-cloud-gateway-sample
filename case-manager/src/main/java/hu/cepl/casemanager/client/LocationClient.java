76dee71068993ffb7ea83a17580eb630d2e08964
Erdőhegyi László <laszlo.erdohegyi@gmail.com>
2025-04-06 14:05:17 +0200

    Fix missing annotations and wrong property name

--- case-manager/src/main/java/hu/cepl/casemanager/client/LocationClient.java
+++ case-manager/src/main/java/hu/cepl/casemanager/client/LocationClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "${api.gateway.eureka.id}", configuration = FeignConfig.class)
+@FeignClient(name = "${location-manager.eureka.id}", configuration = FeignConfig.class)
 public interface LocationClient {
 
     @GetMapping("/api/location/{id}")
