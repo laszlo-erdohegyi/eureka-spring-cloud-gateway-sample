f01356bd954707e765191ab78850599ef76e4855
Erdőhegyi László <laszlo.erdohegyi@gmail.com>
2025-04-06 13:51:34 +0200

    Services now send their name in the X-Service-Name header when they call another service through FeignClient

--- case-manager/src/main/java/hu/cepl/casemanager/client/LocationClient.java
+++ case-manager/src/main/java/hu/cepl/casemanager/client/LocationClient.java
@@ -1,12 +1,12 @@
-package hu.cepl.personmanager.client;
+package hu.cepl.casemanager.client;
 
-import hu.cepl.personmanager.config.FeignConfig;
-import hu.cepl.personmanager.model.Location;
+import hu.cepl.casemanager.config.FeignConfig;
+import hu.cepl.casemanager.model.Location;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "${location-manager.eureka.id}", configuration = FeignConfig.class)
+@FeignClient(name = "${api.gateway.eureka.id}", configuration = FeignConfig.class)
 public interface LocationClient {
 
     @GetMapping("/api/location/{id}")
--- event-manager/src/main/java/hu/cepl/eventmanager/client/LocationClient.java
+++ event-manager/src/main/java/hu/cepl/eventmanager/client/LocationClient.java
@@ -1,11 +1,12 @@
 package hu.cepl.eventmanager.client;
 
+import hu.cepl.eventmanager.config.FeignConfig;
 import hu.cepl.eventmanager.model.Location;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "${location-manager.eureka.id}")
+@FeignClient(name = "${location-manager.eureka.id}", configuration = FeignConfig.class)
 public interface LocationClient {
 
     @GetMapping("/api/location/{id}")
