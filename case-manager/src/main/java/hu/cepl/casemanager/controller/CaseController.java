c31c7eb35fe97fd8d4a14486d58f64fd6032d1ba
Erdőhegyi László <laszlo.erdohegyi@gmail.com>
2025-04-06 13:52:16 +0200

    location-manager can be called from case-manager for demo purposes

--- case-manager/src/main/java/hu/cepl/casemanager/controller/CaseController.java
+++ case-manager/src/main/java/hu/cepl/casemanager/controller/CaseController.java
@@ -1,6 +1,8 @@
 package hu.cepl.casemanager.controller;
 
+import hu.cepl.casemanager.client.LocationClient;
 import hu.cepl.casemanager.model.Case;
+import hu.cepl.casemanager.model.Location;
 import hu.cepl.casemanager.service.CaseService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,9 +16,11 @@ import java.util.List;
 public class CaseController {
 
     private final CaseService caseService;
+    private final LocationClient locationClient;
 
-    public CaseController(CaseService caseService) {
+    public CaseController(CaseService caseService, LocationClient locationClient) {
         this.caseService = caseService;
+        this.locationClient = locationClient;
     }
 
     @GetMapping("/")
@@ -27,6 +31,11 @@ public class CaseController {
     @GetMapping("/{id}")
     public Case getById(@PathVariable Long id) {
         return caseService.findById(id);
+    }
+
+    @GetMapping("/call-location-manager/{id}")
+    public Location callLocationManager(@PathVariable Long id) {
+        return locationClient.getById(id);
     }
 
 }
