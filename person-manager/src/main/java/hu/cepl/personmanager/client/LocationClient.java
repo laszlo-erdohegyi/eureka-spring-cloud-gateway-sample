7d5153ff482d768ab26e5ef538eef2d55cdd85cc
Erdőhegyi László <laszlo.erdohegyi@gmail.com>
2025-04-06 13:45:12 +0200

    Services call location-manager directly with Eureka instead of calling the api-gateway

--- person-manager/src/main/java/hu/cepl/personmanager/client/LocationClient.java
+++ person-manager/src/main/java/hu/cepl/personmanager/client/LocationClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "${api.gateway.eureka.id}")
+@FeignClient(name = "${location-manager.eureka.id}")
 public interface LocationClient {
 
     @GetMapping("/api/location/{id}")
